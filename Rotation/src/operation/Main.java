8253b5062d87383f27b0f736eafd1a668acbdeb5
Ari Mindell <amindell11@gmail.com>
2015-03-31 11:10:21 -0400

    Created utility methods in Main class

--- Rotation/src/operation/Main.java
+++ Rotation/src/operation/Main.java
@@ -1,11 +1,44 @@
 package operation;
 
+import java.io.File;
+
+import filemanagers.readers.Database;
 import application.Overlay;
 
 public class Main {
-
+	static final String ablInfoPath = "XML";
+	static final int updateTime = 500;
+	private static Sequencer seq;
 	public static void main(String[] args) {
 		Overlay.launch(args);
+		Database.indexFileNames(ablInfoPath);
+		while (Overlay.isOpen()) {
+			update();
+			try {
+				Thread.sleep(updateTime);
+			} catch (InterruptedException e) {
+				System.err.println("update timer was interrupted");
+			}
+		}
+	}
+
+	public static void start() {
+		seq.start();
+	}
+
+	public static void pause() {
+		seq.pause();
+	}
+	public static void reset(){
+		seq.reset();
+	}
+	public static void load(File file){
+		seq.init(file);
+	}
+	public static void update(){
+		Overlay.display(seq.getQue());
+		if(seq.shouldFlash())Overlay.flash();
+		Overlay.displayTimers(seq.getQueTimes());
 	}
 
 }

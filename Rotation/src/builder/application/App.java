534aee79b47e26630379dff72217716b4e336e4e
Ari Mindell <amindell11@gmail.com>
2015-04-14 10:47:24 -0400

    integrated teacher and builder

---
+++ Rotation/src/builder/application/App.java
@@ -0,0 +1,65 @@
+package builder.application;
+
+import java.io.File;
+import java.util.HashMap;
+
+import util.Ability;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import util.filemanagers.readers.XMLManager;
+/**
+ *
+ */
+public class App extends Application {
+	private static String baseClass;
+	private static String advancedClass;
+	private static String tree;
+	
+	public static void main(String[] args){
+		System.out.println("hello");
+		baseClass="Knight";
+		advancedClass="Guardian";
+		tree="Foca";
+		launch();
+	}
+	@Override
+	public void start(Stage stage) {
+		System.out.println("hello");
+		ChoiceBox classChooser=new ChoiceBox<String>();
+		ChoiceBox specChooser=new ChoiceBox<String>();
+		ScrollPane scroll=new ScrollPane();
+		VBox abilityList= new VBox();
+		HashMap<String,Ability> abilities=XMLManager.getAbilityFileNames(new File("XML/"+baseClass+".xml"),advancedClass,tree);
+		for(String s:abilities.keySet()){
+			HBox ability=new HBox();
+			System.out.println(abilities.get(s));
+			ImageView icon=new ImageView(abilities.get(s).getIcon());
+			Label abilityName=new Label(s);
+			ability.setAlignment(Pos.CENTER_LEFT);
+			ability.setSpacing(5);
+			Tooltip tip=new Tooltip(abilities.get(s).getDescription());
+			tip.setMaxWidth(200);
+			tip.setWrapText(true);
+			Tooltip.install(ability, tip);
+			ability.getChildren().addAll(icon,abilityName);
+			abilityList.getChildren().add(ability);
+		}
+		abilityList.setSpacing(10);
+		scroll.setMaxHeight(300);
+		scroll.setContent(abilityList);
+		Scene scene=new Scene(scroll);
+		scene.getStylesheets().add("application/application.css");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
---
+++ Rotation/src/util/filemanagers/readers/XMLManager.java
@@ -0,0 +1,175 @@
+package util.filemanagers.readers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.lang.model.util.Elements;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import util.Ability;
+public class XMLManager {
+	public static HashMap getAbilityFileNames(File file) {
+		HashMap map = new HashMap<String, String>();
+		if (file.getName().contains(".xml")) {
+			try {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+						.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(file);
+				doc.getDocumentElement().normalize();
+				NodeList nList = doc.getElementsByTagName("Ability");
+				for (int temp = 0; temp < nList.getLength(); temp++) {
+					Node nNode = nList.item(temp);
+					Element eElement = (Element) nNode;
+					String key = eElement.getElementsByTagName("Label").item(0)
+							.getAttributes().item(0).getTextContent();
+					// key = key.toLowerCase();
+					// double cooldown=
+					// Double.parseDouble(eElement.getElementsByTagName("Cooldown")
+					// .item(0).getAttributes().item(0).getTextContent());
+					String icon = eElement.getElementsByTagName("Icon").item(0)
+							.getAttributes().item(0).getTextContent()
+							.toLowerCase();
+					String description = eElement
+							.getElementsByTagName("Description").item(0)
+							.getAttributes().item(0).getTextContent();
+					long logID = Long.parseLong(eElement
+							.getElementsByTagName("CombatLogID").item(0)
+							.getAttributes().item(0).getTextContent());
+					Ability a = new Ability(0, key, icon, description, logID);
+					map.put(key, a);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
+	public static HashMap indexClasses(File...files){
+		HashMap map=new HashMap();
+		for (File file:files){
+			if (file.getName().contains(".xml")) {
+				Document doc = getXMLDoc(file);
+				String base=doc.getElementsByTagName("Class").item(0).getAttributes().item(0).getTextContent();
+				NodeList classes=doc.getElementsByTagName("AdvancedClass");
+				for(int x=0;x<classes.getLength();x++){
+					map.put(classes.item(x).getAttributes().item(x).getTextContent(),base);
+				}
+			}
+		}
+		return map;
+	}
+	public static HashMap indexTrees(File...files){
+		HashMap map=new HashMap();
+		for (File file:files){
+			if (file.getName().contains(".xml")) {
+				Document doc = getXMLDoc(file);
+				NodeList classes=doc.getElementsByTagName("AdvancedClass");
+				for(int x=0;x<classes.getLength();x++){
+					NodeList trees=doc.getElementsByTagName("DisciplineTree");
+					for(int y=0;y<classes.getLength();y++){
+					map.put(classes.item(x).getAttributes().item(x).getTextContent(),trees.item(y).getAttributes().item(y).getTextContent());
+					}
+				}
+			}
+		}
+		return map;
+	}
+
+	public static HashMap getAbilityFileNames(File file, String advancedClass,
+			String tree) {
+		System.out.println(true);
+		HashMap map = new HashMap<String, String>();
+		Document doc = getXMLDoc(file);
+		NodeList base = enterNode(doc.getDocumentElement(), "AdvClass", advancedClass);
+		System.out.println(true);
+		NodeList nList = enterNode((Element) base, "DisciplineTree", tree);
+		map.putAll(getAbilitiesInNode(nList));
+		nList=enterNode(doc.getDocumentElement(),"base");
+		System.out.println(nList);
+		map.putAll(getAbilitiesInNode(nList));
+		return map;
+	}
+
+	private static NodeList enterNode(Element base,String tag) {
+		NodeList bList = base.getElementsByTagName(tag);
+		return bList.item(0).getChildNodes();
+
+	}
+
+	public static Document getXMLDoc(File file) {
+		if (file.getName().contains(".xml")) {
+			try {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+						.newInstance();
+				DocumentBuilder dBuilder;
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(file);
+				doc.getDocumentElement().normalize();
+				return doc;
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+			} catch (SAXException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.err.println("File " + file.getPath()
+				+ "is not a valid XML document or does not exist");
+		return null;
+	}
+
+	public static NodeList enterNode(Element base, String tag,
+			String query) {
+		NodeList bList = base.getElementsByTagName(tag);
+		String textContent = "";
+		int x =-1;
+		while (!textContent.equalsIgnoreCase(query)) {
+			x++;
+			textContent = bList.item(x).getAttributes().item(0)
+					.getTextContent();
+		}
+		return bList.item(x).getChildNodes();
+	}
+
+	public static HashMap getAbilitiesInNode(NodeList doc) {
+		HashMap map=new HashMap();
+		NodeList nList = ((Element) doc).getElementsByTagName("Ability");
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			Element eElement = (Element) nNode;
+			if (!eElement.getElementsByTagName("Passive").item(0)
+					.getAttributes().item(0).getTextContent().equals("True")) {
+				String key = eElement.getElementsByTagName("Label").item(0)
+						.getAttributes().item(0).getTextContent();
+				System.out.println(key);
+				// key = key.toLowerCase();
+				// double cooldown=
+				// Double.parseDouble(eElement.getElementsByTagName("Cooldown")
+				// .item(0).getAttributes().item(0).getTextContent());
+				String icon = eElement.getElementsByTagName("Icon").item(0)
+						.getAttributes().item(0).getTextContent().toLowerCase();
+				String description = eElement
+						.getElementsByTagName("Description").item(0)
+						.getAttributes().item(0).getTextContent();
+				long logID = Long.parseLong(eElement
+						.getElementsByTagName("CombatLogID").item(0)
+						.getAttributes().item(0).getTextContent());
+				Ability a = new Ability(0, key, icon, description, logID);
+				System.out.println(key);
+				map.put(key, a);
+			}
+		}
+		return map;
+	}
+
+}
